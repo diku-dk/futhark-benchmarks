75de6599a23b222821d00bf14e6c3b844cdf53e2
Troels Henriksen <athas@sigkill.dk>
2016-01-10 23:34:12 +0100

    Fixes

--- accelerate/mandelbrot-run.py
+++ accelerate/mandelbrot-run.py
@@ -7,10 +7,10 @@ import numpy
 def test_write():
     width=1000
     height=1000
-    image=mandelbrot.main(height, width, 50, -2.5, -1.5, 1.5, 1.5)
+    image=mandelbrot.main(width, height, 50, -2.5, -1.5, 1.5, 1.5)
     name='mandelbrot.png'
     f = open(name, 'wb')
-    w = png.Writer(height, width, greyscale=False, alpha=False, bitdepth=8)
+    w = png.Writer(width, height, greyscale=False, alpha=False, bitdepth=8)
     w.write(f, numpy.reshape(image, (height, width*3)))
 
 
