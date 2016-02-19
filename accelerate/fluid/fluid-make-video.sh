8beb4955846ec98d66869b830e86ce8fa49041b3
Niels G. W. Serup <ngws@metanohi.name>
2016-02-19 13:08:28 +0100

    fluid: Support video generation

---
+++ accelerate/fluid/fluid-make-video.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e # Exit on first error.
+
+cd "$(dirname "$0")"
+
+images_dir="$1"
+
+if ! [ "$images_dir" ]; then
+    echo 'error: images directory must be given as the first argument' > /dev/stderr
+    exit 1
+fi
+
+if ! [ -d "$images_dir" ]; then
+    echo 'error: images directory does not exist' > /dev/stderr
+    exit 1
+fi
+
+ffmpeg -i "$images_dir/fluid_%08d.png" -r 25 \
+       "$(echo "$images_dir" | sed 's/\/$//').webm"
+
+exit 0
