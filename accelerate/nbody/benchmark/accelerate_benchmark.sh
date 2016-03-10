fd37f53306cc2fcd72879cb42ccbc3fd116f5227
Niels G. W. Serup <ngws@metanohi.name>
2016-03-10 14:01:04 +0100

    Add benchmarking data sets to nbody
    Former-commit-id: 56c296a427a76067478445ae958cdce94d656c4a

---
+++ accelerate/nbody/benchmark/accelerate_benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+accelerate_benchmark() {
+    n_bodies="$1"
+    time_step="$2"
+    epsilon="$3"
+    
+    echo "Accelerate with n_bodies=$n_bodies, time_step=$time_step, and epsilon=$epsilon."
+    accelerate-nbody --test=False --benchmark=True \
+                     --bodies="$n_bodies" \
+                     --timestep="$time_step" \
+                     --epsilon="$epsilon"
+}
+
+accelerate_benchmark 100 1.0 50.0
+accelerate_benchmark 1000 1.0 50.0
