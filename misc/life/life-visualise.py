d2ec2a6dc63c7f54f90700fcc5ad51f1740df327
Troels Henriksen <athas@sigkill.dk>
2016-04-15 14:51:51 +0200

    Now with quadlife.
    Former-commit-id: 1a33605c4a4502b78e6d97749d64e37c3441faa4

--- misc/life/life-visualise.py
+++ misc/life/life-visualise.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import life as futlife
+import life
+import quadlife
 import numpy
 import pygame
 import time
@@ -11,20 +12,23 @@ width=800
 height=600
 size=(width,height)
 frame_every=1.0/30.0
+
+# TODO: add command line parameter for picking between life and quadlife.
+
 def main():
-    life=futlife.life()
+#    l=life.life()
+    l=quadlife.quadlife()
     screen = pygame.display.set_mode(size)
     initworld = numpy.random.choice([True, False], size=size)
-    world, history = life.init(initworld)
+    world, history = l.init(initworld)
     surface = pygame.Surface(size)
     screen.blit(surface, (0, 0))
 
     last_frame = time.time()
     while True:
-        world, history = life.steps(world, history, STEPS_PER_RUN)
-
+        world, history = l.steps(world, history, STEPS_PER_RUN)
         if time.time() > last_frame + frame_every:
-            frame = life.render_frame(history)
+            frame = l.render_frame(history)
             pygame.surfarray.blit_array(surface, frame)
             screen.blit(surface, (0, 0))
             pygame.display.flip()
