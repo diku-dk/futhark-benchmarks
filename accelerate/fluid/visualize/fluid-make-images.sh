82bba27a2d02397402be7a1e6ac10f0a5b8cff33
Niels G. W. Serup <ngws@metanohi.name>
2016-04-15 15:59:29 +0200

    Fix fluid visualization image generation script.

--- accelerate/fluid/visualize/fluid-make-images.sh
+++ accelerate/fluid/visualize/fluid-make-images.sh
@@ -6,9 +6,10 @@ cd "$(dirname "$0")"
 
 n_steps="$1"
 grid_resolution="$2"
-backend="$3"
-out_dir="$4"
-seed="$5"
+n_solver_steps="$3"
+backend="$4"
+out_dir="$5"
+seed="$6"
 
 if ! [ "$n_steps" ]; then
     echo 'error: the number of steps must be given as the first argument' > /dev/stderr
@@ -20,18 +21,23 @@ if ! [ "$grid_resolution" ]; then
     exit 1
 fi
 
+if ! [ "$n_solver_steps" ]; then
+    echo 'error: the number of lin_solve steps must be given as the third argument' > /dev/stderr
+    exit 1
+fi
+
 if ! [ "$backend" ]; then
-    echo 'error: backend ("futhark" or "c") must be given as the third argument' > /dev/stderr
+    echo 'error: backend ("futhark" or "c") must be given as the fourth argument' > /dev/stderr
     exit 1
 fi
 
 if ! [ "$out_dir" ]; then
-    echo 'error: output directory must be given as the fourth argument' > /dev/stderr
+    echo 'error: output directory must be given as the fifth argument' > /dev/stderr
     exit 1
 fi
 
 if ! [ "$seed" ]; then
-    echo 'error: seed (number or "none") must be given as the fifth argument' > /dev/stderr
+    echo 'error: seed (number or "none") must be given as the sixth argument' > /dev/stderr
     exit 1
 fi
 
@@ -45,7 +51,7 @@ if ! [ -e "../src-$backend/fluid-visualize-densities" ]; then
     exit 1
 fi
 
-../fluid-generate-random-input.py "$n_steps" "$grid_resolution" "$backend" "$seed" \
+../fluid-generate-random-input.py "$n_steps" "$grid_resolution" "$n_solver_steps" "$backend" "$seed" \
     | ../src-"$backend"/fluid-visualize-densities \
     | ./fluid-output-to-images.py "$out_dir" "$backend"
 
