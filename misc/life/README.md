59a2172e56e7339aa39a7dec36be9351f72e0875
Troels Henriksen <athas@sigkill.dk>
2016-04-16 08:28:32 +0200

    Some documentation.

---
+++ misc/life/README.md
@@ -0,0 +1,14 @@
+== Game of Life in Futhark
+
+Just run `make` to build, then `python life-visualise.py`.  Requires
+the Futhark compiler, Pygame and PyOpenCL.  And of course, a working
+OpenCL setup.
+
+The program is not interactive, so just look at the pretty colours.
+When a cell dies, it will slowly fade out rather than turning white
+immediately, leading to interesting explosion-like effects.
+
+A number of command line options are supported.  To run with an
+alternative ruleset using four different cells, try:
+
+    python life-visualise --variant quadlife
--- misc/life/life-visualise.py
+++ misc/life/life-visualise.py
@@ -4,35 +4,46 @@ import life
 import quadlife
 import numpy
 import pygame
-import time
+import argparse
 import pyopencl as cl
+import sys
 
-STEPS_PER_RUN=3
-width=800
-height=600
-size=(width,height)
-frame_every=1.0/30.0
+rulesets = {
+    'life' : life.life,
+    'quadlife' : quadlife.quadlife
+}
 
-# TODO: add command line parameter for picking between life and quadlife.
+parser = argparse.ArgumentParser(description='The Game of Life!')
+parser.add_argument('--variant', metavar='RULES', type=str, choices=rulesets.keys(), default='life',
+                    help='The variant of the game to run')
+parser.add_argument('--width', metavar='INT', type=int, default=800,
+                    help='Width of the world')
+parser.add_argument('--height', metavar='INT', type=int, default=600,
+                    help='Height of the world')
+parser.add_argument('--steps', metavar='INT', type=int, default=3,
+                    help='Number of simulation steps to perform per frame')
 
-def main():
-#    l=life.life()
-    l=quadlife.quadlife()
-    screen = pygame.display.set_mode(size)
-    initworld = numpy.random.choice([True, False], size=size)
-    world, history = l.init(initworld)
-    surface = pygame.Surface(size)
+args = parser.parse_args()
+
+steps=args.steps
+size=(args.width,args.height)
+
+
+l = rulesets[args.variant]()
+screen = pygame.display.set_mode(size)
+initworld = numpy.random.choice([True, False], size=size)
+world, history = l.init(initworld)
+surface = pygame.Surface(size)
+
+def render():
+    frame = l.render_frame(history)
+    pygame.surfarray.blit_array(surface, frame)
     screen.blit(surface, (0, 0))
+    pygame.display.flip()
 
-    last_frame = time.time()
-    while True:
-        world, history = l.steps(world, history, STEPS_PER_RUN)
-        if time.time() > last_frame + frame_every:
-            frame = l.render_frame(history)
-            pygame.surfarray.blit_array(surface, frame)
-            screen.blit(surface, (0, 0))
-            pygame.display.flip()
-            last_frame = time.time()
-
-if __name__ == '__main__':
-    main()
+while True:
+    world, history = l.steps(world, history, steps)
+    render()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
