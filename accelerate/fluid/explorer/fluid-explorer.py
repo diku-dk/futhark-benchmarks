197137685607c5e00bb9cdc358b79546c7ccae8a
Troels Henriksen <athas@sigkill.dk>
2016-04-18 08:02:39 +0200

    Smoother line drawing.

--- accelerate/fluid/explorer/fluid-explorer.py
+++ accelerate/fluid/explorer/fluid-explorer.py
@@ -9,6 +9,63 @@ import pygame
 
 import fluid
 
+# From http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm#Python
+def get_line(start, end):
+    """Bresenham's Line Algorithm
+    Produces a list of tuples from start and end
+
+    >>> points1 = get_line((0, 0), (3, 4))
+    >>> points2 = get_line((3, 4), (0, 0))
+    >>> assert(set(points1) == set(points2))
+    >>> print points1
+    [(0, 0), (1, 1), (1, 2), (2, 3), (3, 4)]
+    >>> print points2
+    [(3, 4), (2, 3), (1, 2), (1, 1), (0, 0)]
+    """
+    # Setup initial conditions
+    x1, y1 = start
+    x2, y2 = end
+    dx = x2 - x1
+    dy = y2 - y1
+
+    # Determine how steep the line is
+    is_steep = abs(dy) > abs(dx)
+
+    # Rotate line
+    if is_steep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    # Swap start and end points if necessary and store swap state
+    swapped = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        swapped = True
+
+    # Recalculate differentials
+    dx = x2 - x1
+    dy = y2 - y1
+
+    # Calculate error
+    error = int(dx / 2.0)
+    ystep = 1 if y1 < y2 else -1
+
+    # Iterate over bounding box generating points between start and end
+    y = y1
+    points = []
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if is_steep else (x, y)
+        points.append(coord)
+        error -= abs(dy)
+        if error < 0:
+            y += ystep
+            error += dx
+
+    # Reverse the list if the coordinates were swapped
+    if swapped:
+        points.reverse()
+    return points
 
 class FluidQuit(Exception):
     pass
@@ -63,12 +120,14 @@ class FluidExplorer:
         self.U[x1, y1] = force * (x1 - x0)
         self.V[x1, y1] = force * (y1 - y0)
 
-    def add_density(self, pos):
-        x, y = pos
+    def add_density(self, pos_old, pos_new):
+        x_old, y_old = pos_old
+        x_new, y_new = pos_new
         source = 100.0
-        x_ = x + 1
-        y_ = y + 1
-        self.D[x_, y_] = source
+        points = get_line((x_old+1, y_old+1),
+                          (x_new+1, y_new+1))
+        for x,y in points:
+            self.D[x,y] = source
 
     def react(self):
         for event in pygame.event.get():
@@ -87,10 +146,11 @@ class FluidExplorer:
 
         pos_new = pygame.mouse.get_pos()
 
-        if self.making_densities:
-            self.add_density(pos_new)
-        elif self.making_forces and self.pos_old is not None:
-            self.add_force(self.pos_old, pos_new)
+        if self.pos_old is not None:
+            if self.making_densities:
+                self.add_density(self.pos_old, pos_new)
+            elif self.making_forces and self.pos_old is not None:
+                self.add_force(self.pos_old, pos_new)
 
         self.pos_old = pos_new
 
