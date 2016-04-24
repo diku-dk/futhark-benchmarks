4794a35df6c81389271319d98724272fab73f6db
Niels G. W. Serup <ngws@metanohi.name>
2016-04-24 23:46:41 +0200

    Fix Mandelbrot explorer to still work with the NumPy backend.
    Former-commit-id: 180658d6cecf2e6e0ba2e75f8a81f04adee392c9

--- misc/mandelbrot-explorer/mandelbrot-explorer.py
+++ misc/mandelbrot-explorer/mandelbrot-explorer.py
@@ -17,7 +17,7 @@ startpos=(-2.23,-1.15,0.83,1.15)
 futm = mandelbrot.mandelbrot()
 
 def make_mandelbrot_futhark(minx, miny, maxx, maxy):
-    return futm.main(width, height, limit, minx, miny, maxx, maxy)
+    return futm.main(width, height, limit, minx, miny, maxx, maxy).get()
 
 def make_mandelbrot_numpy(minx, miny, maxx, maxy):
     return mandelbrot_numpy.mandelbrot(width, height, limit, minx, miny, maxx, maxy)
@@ -117,7 +117,7 @@ def showText(what, where):
 
 def render():
     start = time.time()
-    frame = make_mandelbrot(minx, miny, maxx, maxy).get()
+    frame = make_mandelbrot(minx, miny, maxx, maxy)
     end = time.time()
     pygame.surfarray.blit_array(surface, frame)
     screen.blit(surface, (0, 0))
