b9e86c6b9ebad84c0d6182147a9b6491f88769aa
Troels Henriksen <athas@sigkill.dk>
2016-08-18 22:50:21 +0200

    Another pretty program.

---
+++ accelerate/tunnel/tunnel-gui.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import tunnel
+
+import numpy
+import pygame
+import time
+import sys
+
+width=1200
+height=800
+size=(width,height)
+
+render_tunnel = tunnel.tunnel().main
+
+pygame.init()
+pygame.display.set_caption('Tunnel')
+screen = pygame.display.set_mode(size)
+surface = pygame.Surface(size)
+font = pygame.font.Font(None, 36)
+
+curtime=0
+
+def showText(what, where):
+    text = font.render(what, 1, (255, 255, 255))
+    screen.blit(text, where)
+
+def render():
+    start = time.time()
+    frame = render_tunnel(curtime, width, height).get()
+    end = time.time()
+    pygame.surfarray.blit_array(surface, frame)
+    screen.blit(surface, (0, 0))
+
+    speedmessage = "Futhark call took %.2fms" % ((end-start)*1000)
+    showText(speedmessage, (10, 10))
+
+    pygame.display.flip()
+
+while True:
+    curtime += 0.005
+    render()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
