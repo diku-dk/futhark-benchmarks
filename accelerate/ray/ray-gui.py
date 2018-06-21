9ab7f52fdd73a1a66f01a760334bfdc09330e677
Troels Henriksen <athas@sigkill.dk>
2018-06-21 15:11:42 +0200

    Nicer interface.

--- accelerate/ray/ray-gui.py
+++ accelerate/ray/ray-gui.py
@@ -2,7 +2,7 @@
 
 import trace
 
-import numpy
+import numpy as np
 import pygame
 import time
 import sys
@@ -28,14 +28,20 @@ def showText(what, where):
     text = font.render(what, 1, (255, 255, 255))
     screen.blit(text, where)
 
+tcur = 0.0
+tlast = time.time()
+paused = False
+
 def render(eye):
-    global rtime
-    rtime += 0.01
+    global tlast, tcur
     start = time.time()
+    tdelta = start - tlast if not paused else 0
+    tcur += tdelta
+    tlast = start
     frame = raytracer.main(width,height,fov,
                            eye['point']['x'], eye['point']['y'], eye['point']['z'],
                            eye['vector']['x'], eye['vector']['y'], eye['vector']['z'],
-                           bouncelimit,rtime).get()
+                           bouncelimit,np.float32(tcur)).get()
     end = time.time()
     pygame.surfarray.blit_array(surface, frame)
     screen.blit(surface, (0, 0))
@@ -57,17 +63,19 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused
             if event.key == pygame.K_RIGHT:
                 eye['point']['x'] += 1
             if event.key == pygame.K_LEFT:
                 eye['point']['x'] -= 1
-            if event.key == pygame.K_UP:
-                eye['point']['y'] -= 1
-            if event.key == pygame.K_DOWN:
-                eye['point']['y'] += 1
-            if event.key == pygame.K_PAGEDOWN:
-                eye['point']['z'] -= 1
             if event.key == pygame.K_PAGEUP:
+                eye['point']['y'] -= 1
+            if event.key == pygame.K_PAGEDOWN:
+                eye['point']['y'] += 1
+            if event.key == pygame.K_DOWN:
+                eye['point']['z'] -= 1
+            if event.key == pygame.K_UP:
                 eye['point']['z'] += 1
             if event.key == pygame.K_HOME:
                 eye = orig_eye
