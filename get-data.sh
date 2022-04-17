87539c8c9121025cd671182306ee489a68f11588
Philip Munksgaard <philip@munksgaard.me>
2022-04-17 13:03:25 +0200

    Use get-data.sh to manage datasets from ERDA
    This removes the dependency on git-annex and greatly simplifies that management
    of external data.
    
    The idea is to maintain an external-data.txt file which contains lines of the
    form:
    
    OUTPUT URL SHA
    
    where OUTPUT is the path of the file to manage with get-data.sh, preferably
    something in .external-data, which has been added to .gitignore. URL is the
    url to the data hosted on ERDA and the SHA is the SHA256 checksum of the file.
    
    After checking out the repository, simply run ./get-data.sh external-data.txt
    to download the data dependencies.

---
+++ get-data.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# usage: get-data.sh external-data.txt
+#
+# external-data.txt file must contain lines of the format:
+#
+#   PATH URL SHA256SUM
+#
+# get-data.sh will attempt to download the file at URL into PATH (relative to
+# the location of external-data.txt) after verifying that the sha256sum is
+# identical to SHA256SUM. Neither field can contain spaces.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ "$#" -ne "1" ]; then
+    echo "Usage: $0 FILE"
+    echo "FILE must be a file containing lines of the format:"
+    echo "   PATH URL SHA256SUM"
+    echo "$0 will attempt to download the file at URL into PATH (relative to"
+    echo "the location of external-data.txt) after verifying that the sha256sum is"
+    echo "identical to SHA256SUM. Neither field can contain spaces."
+
+    exit 3
+fi
+
+if [ -z "$(which sha256sum)" ]; then
+    echo "Error: sha256sum could not be found."
+
+    exit 4
+fi
+
+if [ -z "$(which curl)" ]; then
+    echo "Error: curl could not be found."
+
+    exit 5
+fi
+
+BASEDIR=$(dirname "$1")
+
+while read -r OUTPUT URL CHECKSUM; do
+    echo "Now processing $OUTPUT..."
+
+    if [ -f "$OUTPUT" ]; then
+        COMPUTED_SUM=$(sha256sum "$OUTPUT" | cut --fields=1 --delimiter=' ')
+        if [ "$COMPUTED_SUM" = "$CHECKSUM" ]; then
+            echo "File exists. Skipping."
+            continue
+        else
+            echo "Error: File exists but has invalid checksum!"
+            echo "Expected $CHECKSUM, got $COMPUTED_SUM."
+            echo "You can manually delete the file to get the correct version."
+            exit 2
+        fi
+    fi
+
+    TMPFILE=$(mktemp)
+    curl --fail "$URL" --output "$TMPFILE"
+    COMPUTED_SUM=$(sha256sum "$TMPFILE" | cut --fields=1 --delimiter=' ')
+
+    if [ "$COMPUTED_SUM" = "$CHECKSUM" ]; then
+        mkdir --parents "${BASEDIR}/$(dirname "$OUTPUT")"
+        mv "$TMPFILE" "${BASEDIR}/${OUTPUT}"
+    else
+        echo "Error: Invalid checksum of downloaded file!"
+        echo "Expected $CHECKSUM, got $COMPUTED_SUM."
+        exit 1
+    fi
+done < "$1"
+
+echo "Done."
